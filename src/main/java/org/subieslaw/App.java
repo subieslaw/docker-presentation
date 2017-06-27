545aa0a73faf10f784b452d8aa22fbd856515be6
Pawel Urbanik <pawel.urbanik@bbh.com>
2017-06-27 11:26:42 +0200

    initial project structure and minimal pom

---
+++ src/main/java/org/subieslaw/App.java
@@ -0,0 +1,13 @@
+package org.subieslaw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
