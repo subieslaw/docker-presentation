360e82c050126b65fbd97683a01b67a6f06aafae
Pawel Urbanik <subieslaw@gmail.com>
2017-06-27 22:04:24 +0200

    simple rest service

--- src/main/java/org/subieslaw/App.java
+++ src/main/java/org/subieslaw/App.java
@@ -1,13 +1,20 @@
 package org.subieslaw;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.subieslaw.domain.KYC;
+import org.subieslaw.domain.KYCBuilder;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("kycrulez")
+public class App {
+
+    @GET
+    @Produces("application/json")
+    public KYC getHtml(@PathParam("kyc") String clientName) {
+        return new KYCBuilder().setName(clientName).createKYC();
     }
+
 }
