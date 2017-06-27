573208c8dea83f980f50361921bcec7409653d6e
Pawel Urbanik <subieslaw@gmail.com>
2017-06-27 23:02:39 +0200

    application annotataion has been added

--- src/main/java/org/subieslaw/App.java
+++ src/main/java/org/subieslaw/App.java
@@ -3,11 +3,9 @@ package org.subieslaw;
 import org.subieslaw.domain.KYC;
 import org.subieslaw.domain.KYCBuilder;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 
+@ApplicationPath("/")
 @Path("kycrulez")
 public class App {
 
